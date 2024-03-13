e8500dbb0c153727ae6f217c116e2192fe1286fa
Anubhav Pabby <anubhavpabby@gmail.com>
2024-03-13 20:13:51 +0530

    Structured code in HelloController

--- src/main/java/com/anubhavpabby/springbootbasics/controller/HelloController.java
+++ src/main/java/com/anubhavpabby/springbootbasics/controller/HelloController.java
@@ -6,14 +6,15 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping(value="/hello")
 public class HelloController {
 
-    @RequestMapping(value = "/api/spring-boot-basics", method = RequestMethod.GET)
+    @RequestMapping(value = "/spring-boot-basics", method = RequestMethod.GET)
     public String basicAppInfo() {
         return "Spring Boot application to learn the basics of Spring Boot";
     }
 
-    @GetMapping("/api/welcome-user")
+    @GetMapping("/welcome-user")
     public String welcomeUser() {
         return "Welcome to Anubhav Pabby's basic Spring Boot Application";
     }
